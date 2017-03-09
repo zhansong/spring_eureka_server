a8e6fcbe57d8d3ef29a27635368a745754234161
zhansong <114837616@qq.com>
2017-03-09 14:15:14 +0800

    1

---
+++ src/main/java/com/ks3/cdn/EurekaServer.java
@@ -0,0 +1,18 @@
+package com.ks3.cdn;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by ZHANSONG on 2017/3/8.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaServer.class).web(true).run(args);
+    }
+
+}
